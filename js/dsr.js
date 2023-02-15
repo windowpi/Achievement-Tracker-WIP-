8828e04d9bd8b698018a2cc6a1fc36924b2aad27
windowpi <justin.ludwick17@gmail.com>
2023-02-14 20:56:40 -0600

    if checkbox checked add number to completed issue with total number of achievements in card-body

--- js/dsr.js
+++ js/dsr.js
@@ -4,4 +4,18 @@
     Date: Started(1-13-23) 
 */
 
+// get input values and add up completed achievements
+var input = document.getElementsByClassName('sum'),
+    total = document.getElementById('achievement-complete');
 
+for (var i = 0; i < input.length; i++) {
+    input[i].onchange = function () {
+        var add = this.value * (this.checked ? 1 : -1);
+        total.innerHTML = parseFloat(total.innerHTML) + add
+    }
+}
+
+// get total input tags in 
+const element = document.getElementsByClassName('card-body');
+const nodes = element.getElementsByTagName('input');
+document.getElementById('achievement-total').innerHTML = nodes.length;

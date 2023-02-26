a1b87fc11efa1d70a383a9e2da85bef3f66aeabb
windowpi <justin.ludwick17@gmail.com>
2023-02-26 14:44:08 -0600

    finally implemented tracking of completed achievements

--- js/dsr.js
+++ js/dsr.js
@@ -4,19 +4,39 @@
     Date: Started(1-13-23) 
 */
 
-// get input values and add up completed achievements
-var input = document.getElementsByClassName('sum'),
-    total = document.getElementById('achievement-complete');
-
-for (var i = 0; i < input.length; i++) {
-    input[i].onchange = function () {
-        var add = this.value * (this.checked ? 1 : -1);
-        total.innerHTML = parseFloat(total.innerHTML) + add
-        console.log(add);
-    }
-}
 
 // get total input tags in 
-const element = document.getElementsByClassName('card-body')[0];
-const nodes = element.getElementsByTagName('input');
-document.getElementById('achievement-total').innerHTML = nodes.length;
+//const element = document.getElementsByClassName('card-body')[0];
+//const nodes = element.getElementsByTagName('input');
+//document.getElementById('achievement-total').innerHTML = nodes.length;
+
+// get input values and add up completed achievements
+// Cache container element and add a listener to it
+const container = document.querySelector('.container');
+container.addEventListener('click', handleClick);
+
+function handleClick(e) {
+
+  // Check that the element that fired the event
+  // is a checkbox
+  if (e.target.matches('[type="checkbox"]')) {
+
+    // Destructure the checked attribute from the
+    // checkbox
+    const {
+      checked
+    } = e.target;
+
+    // Find the closest element with a `card-body` class
+    const card = e.target.closest('.card-body');
+
+    // Use that to find the local "achievement" element
+    const achievement = card.querySelector('.achievement-complete');
+
+    // Get and parse its total
+    let total = Number(achievement.textContent);
+
+    // Update the text content with an new total
+    achievement.textContent = checked ? total += 1 : total -= 1;
+  }
+}

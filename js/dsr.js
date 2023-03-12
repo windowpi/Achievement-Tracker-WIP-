a8c179248d94a3ad0bb81bbd7de45c84d64b0aed
windowpi <justin.ludwick17@gmail.com>
2023-03-12 11:46:15 -0500

    added localstorage for checkboxes

--- js/dsr.js
+++ js/dsr.js
@@ -14,9 +14,36 @@ for (var i = 0; i < input.length; i++) {
   }
 }
 
-// get total input tags in 
-//const element = document.getElementsByClassName('card-body')[0];
-//const nodes = element.getElementsByTagName('input');
-//document.getElementById('achievement-total').innerHTML = nodes.length;
+// if checkbox is checked add to .achievement-complete span
+
+// Cache container element and add a listener to it
+const container = document.querySelector('.achievement-list');
+container.addEventListener('click', handleClick);
+
+function handleClick(e) {
+
+  // Check that the element that fired the event is a checkbox
+  if (e.target.matches('[type="checkbox"]')) {
+
+    // Destructure the checked attribute from the
+    // checkbox
+    const {
+      checked
+    } = e.target;
+
+    // Find the closest element with a `card-body` class
+    const card = e.target.closest('.card-body');
+
+    // Use that to find the local "achievement" element
+    const achievement = card.querySelector('.achievement-complete');
+
+    // Get and parse its total
+    let total = Number(achievement.textContent);
+
+    // Update the text content with an new total
+    achievement.textContent = checked ? total += 1 : total -= 1;
+    localStorage.setItem("total", total);
+  }
+}
 
 
